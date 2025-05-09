8a984a4767bf72fed4475df2fcaf304bce86b73a
Nguyen Trung Nghia <nghiabgt@gmail.com>
2025-05-09 09:21:13 +0700

    update slide

--- dashboard/pages/page_management/slide.vue
+++ dashboard/pages/page_management/slide.vue
@@ -224,6 +224,17 @@ const onFilesChange = async e => {
   }
 };
 await loadData();
+onMounted(() => {
+  const settingStore = useSettingStore();
+  const tempBreadcrumb = computed(() => [
+    { url: "/page_management", title: "Quản lý site" },
+    { url: "/page_management/slide", title: "Quản lý banner" },
+  ]);
+  settingStore.setBreadcrumb(tempBreadcrumb.value);
+  watch(tempBreadcrumb, () => {
+    settingStore.setBreadcrumb(tempBreadcrumb.value);
+  });
+});
 </script>
 
 <style scoped></style>
